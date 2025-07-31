f72c66d3487142edda1147a34823a2a01baffd88
DaLaw2 <t20040421@gmail.com>
2025-07-31 22:14:25 +0800

    feat: Add icon and update dependent (#3)
    * fix: Type error on release mode and refactor README.md
    
    * feat: Add icon and update dependent

--- src/platform/linux/elevate.rs
+++ src/platform/linux/elevate.rs
@@ -1,15 +1,19 @@
+use crate::model::error::Error;
+use crate::model::error::system::SystemError;
 use std::process::Command;
 use std::{env, io};
 
-pub fn elevate() -> io::Result<()> {
-    let exe = env::current_exe()?;
+pub fn elevate() -> Result<(), Error> {
+    let exe = env::current_exe()
+        .map_err(|_| SystemError::RunAsAdminFailed)?;
     let args: Vec<String> = env::args().skip(1).collect();
 
     Command::new("sudo")
         .arg("--")
         .arg(exe)
         .args(&args)
-        .status()?;
+        .status()
+        .map_err(|_| SystemError::RunAsAdminFailed)?;
 
     Ok(())
 }
